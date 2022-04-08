670c66499e0d61e4ea18b0fea4d83c6e16140940
Girija Sankar <yagneswarasankar@gmail.com>
2022-04-08 22:50:56 +0530

    First Commit

---
+++ src/ReverseString.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+    //This program will reverse the string input
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string to be reversed");
+        String str = scanner.nextLine();
+        System.out.println("Enter string : "+ str);
+        System.out.println("Reversed String: "+reverseString(str));
+        System.out.println("Reversed String: "+reverseString1(str));
+
+    }
+
+    //Method Take the string in character array.
+    //Append the string to the reverse to the resultant
+    static String reverseString(String str){
+        char[] chrArray = str.toCharArray();
+        String res ="";
+        for(int i = chrArray.length -1; i>= 0;i--){
+            res += chrArray[i];
+        }
+        return res;
+    }
+
+    //charAt will return the character at a certain index.
+    //Traverse the string reverse and assign it to the resultant string
+    static String reverseString1(String str){
+        String resString = "";
+        for(int i = str.length() -1;i >= 0;i-- ){
+            str += str.charAt(i);
+            //System.out.println(resString);
+        }
+        return resString;
+    }
+
+}
